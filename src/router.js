1ccdaacaf2f4660eba54f704f37e9a259c538faa
Mikhail Stefantsev <mikhail.stefantsev@outlook.com>
2021-05-29 13:12:35 +0300

    migrated to local npm registry

--- src/router.js
+++ src/router.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const ObjectID = require('mongodb').ObjectID;
 
-const flash = require('./lib/flash');
+const flash = require('flash-http');
 const openDB = require('./lib/db');
 const setup = require('./lib/setup');
 const socket = require('./lib/socket');
